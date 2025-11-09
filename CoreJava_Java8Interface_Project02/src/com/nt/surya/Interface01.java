7b174a61eeb71d99b84b1ba20d4bc7df9c10d44b
BootWithStream <paikaraysuryakanta1@gmail.com>
2025-11-09 09:37:29 +0530

    Interface example

---
+++ CoreJava_Java8Interface_Project02/src/com/nt/surya/Interface01.java
@@ -0,0 +1,11 @@
+package com.nt.surya;
+
+//@FunctionalInterface
+public interface Interface01 {
+	public void m1();
+	public void m2();
+//	public int m3(){
+//	return 0;	 //it's not allowed because before java 8 interface only had abstract method
+//	}			// But after java 8 interface support both concrete and abstract method using 
+				// default method and static method
+}
